6d14cda11f7bad8fc93365d487a4748f9c1969d8
mkouzios <mkouzios@hotmail.com>
2020-02-17 22:41:04 -0600

    Added Google extension to projects list, minor styling adjustments

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -18,28 +18,41 @@ class Projects extends Component {
                 <div className='sub-section'>
                   <Row className='d-flex justify-content-center'>
                     <h5>
-                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/kouzios/telegram-man-page-bot">Telegram Manual Page Bot</a>
+                      <a href="https://github.com/kouzios/telegram-man-page-bot">Telegram Manual Page Bot</a>
                     </h5>
                   </Row>
                   <Row className='d-flex justify-content-center'>
                     <img src='telegram_man.gif' alt='Telegram manual page bot gif'/>
                   </Row>
                   <Row className='d-flex justify-content-center'>
-                    <span>This is a bot dedicated to getting results from the Linux `man` command, all in your telegram chat!</span>
+                    <span className='description'>This is a bot dedicated to getting results from the Linux `man` command, all in your telegram chat!</span>
                   </Row>
                 </div>
                 <hr/>
                 <div className='sub-section'>
                   <Row className='d-flex justify-content-center'>
                     <h5>
-                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/kouzios/sd_bot">Discord Meeting Scheduler Bot</a>
+                      <a href="https://github.com/kouzios/mtg-card-extention">Discord Meeting Scheduler Bot</a>
                     </h5>
                   </Row>
                   <Row className='d-flex justify-content-center'>
                     <img src='discord_schedule.gif' alt='Discord scheduling bot gif'/>
                   </Row>
                   <Row className='d-flex justify-content-center'>
-                    <span>This is a discord bot that enables you to schedule weekly meetings at a specified time!</span>
+                    <span className='description'>This is a discord bot that enables you to schedule weekly meetings at a specified time!</span>
+                  </Row>
+                </div>
+                <div className='sub-section'>
+                  <Row className='d-flex justify-content-center'>
+                    <h5>
+                      <a href="https://github.com/kouzios/telegram-man-page-bot">Google Chrome MTG Extension</a>
+                    </h5>
+                  </Row>
+                  <Row className='d-flex justify-content-center'>
+                    <img src='extension.png' alt='Magic The Gathering card adding extension'/>
+                  </Row>
+                  <Row className='d-flex justify-content-center'>
+                    <span className='description'>This is a Google Chrome extension acting as a notepad to quickly store Magic The Gathering cards for deck building with a simple right mouse click action!</span>
                   </Row>
                 </div>
               </Col>

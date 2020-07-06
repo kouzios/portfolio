e5e841c6a1e39671776fe2869c1cec1c16b501c0
Matthew Kouzios <mkouzios@hotmail.com>
2020-07-06 19:11:56 -0400

    Optimized performance through non-invisible text and video loadings

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'lazysizes';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -12,19 +13,22 @@ const projects =
       title: "Telegram Manual Page Bot",
       github: "https://github.com/kouzios/telegram-man-page-bot",
       description: "This is a bot dedicated to getting results from the Linux `man` command, all in your telegram chat!",
-      image: "telegram_man.gif"
+      display: "telegram_man.webm",
+      display_type: "video",
     },
     {
       title: "Discord Meeting Scheduler Bot",
       github: "https://github.com/kouzios/mtg-card-extention",
       description: "This is a discord bot that enables you to schedule weekly meetings at a specified time!",
-      image: "discord_schedule.gif"
+      display: "discord_schedule.webm",
+      display_type: "video",
     },
     {
       title: "Google Chrome MTG Extension",
       github: "https://github.com/kouzios/telegram-man-page-bot",
       description: "This is a Google Chrome extension acting as a notepad to quickly store Magic The Gathering cards for deck building with a simple right mouse click action!",
-      image: "extension.jpg"
+      display: "extension.jpg",
+      display_type: "image",
     }
   ];
 
@@ -36,19 +40,24 @@ const Projects = () => (
             <h1 className='title centered-text'>Projects</h1>
         </Row>
         {projects.map((project, index) => (
-          <div className='sub-section'>
+          <div key={"sub-section" + index} className='sub-section'>
             <Row className='mb-2 d-flex justify-content-center'>
               <h5 className='git'><a href={project.github}>{project.title}</a></h5>
             </Row>
             <Row>
               <Col>
-                  <img src={project.image} alt={project.title + ' gif'}/>
+                  {project.display_type === "video" ?
+                    <video controls>
+                      <source src={project.display} alt={project.title + ' video'} type="video/webm" />
+                    </video> 
+                      : <img src={project.display} alt={project.title + ' image '} />
+                  }
               </Col>
               <Col className='d-flex align-items-center'>
                   <span className='description'>{project.description}</span>
               </Col>
             </Row>
-        {index < (projects.length-1) ? <hr/> : null /*Ensure no bottom-most separation bar*/} 
+            {index < (projects.length-1) ? <hr/> : null /*Ensure no bottom-most separation bar*/} 
           </div>
         ))}
       </Col>
